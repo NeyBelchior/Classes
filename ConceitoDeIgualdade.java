ec42f1c93ea6587af70d216520e815a9ff47e030
Ney Belchior <neybelchior@nomus.com.br>
2021-12-06 14:35:50 -0300

    Equals

---
+++ ConceitoDeIgualdade.java
@@ -0,0 +1,35 @@
+package Classes;
+
+public class ConceitoDeIgualdade {
+	
+	/*
+	 * 
+	 * Produto p1 =new Produto();
+	 * p1.nome="Caneta";
+	 * 
+	 * Produto p2 = new Produto();
+	 * p2.nome = "caneta";
+	 * 
+	 * 
+	 * p1 == p2 ?  FALSE
+	 * 
+	 * == compara endereço de memória  e nesse caso são destintos 
+	 * 
+	 * 
+	 * Equals> É um metodo que está disponível em todos os objetos em java
+	 * dentrodo metodo equals se coloca o código(critério) que vai definir se algo é igual a algo 
+	 * 
+	 * Equals deve ser implementa com hashcode
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	
+
+}
