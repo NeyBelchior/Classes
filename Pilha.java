a8cc17d74db2c23344ad9d39c94a1711615d282f
Ney Belchior <neybelchior@nomus.com.br>
2021-12-06 20:15:03 -0300

    pilha

---
+++ Pilha.java
@@ -0,0 +1,40 @@
+package Classes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+
+	public static void main(String[] args) {
+		//conceito de pilha é igual o livro lifo
+		//last in first out
+		
+		Deque<String> livros  = new ArrayDeque<>();
+		livros.add("O pequeno principe");//se add ou nao retorna true ou false
+		livros.push("Don Quixote");// retorna uma execption se o elemento que eu tentar adicionar for restrito por restrições de capacidade
+        livros.push("O Hobbit");
+        
+        System.out.println(livros.peek());
+        System.out.println(livros.element());
+        
+        for(String percorre : livros) {
+        	System.out.println(percorre);
+        }
+        System.out.println(livros.pop());//joga execessão 
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+        //System.out.println(livros.poll());
+      //  System.out.println(livros.remove());//joga execessão 
+        
+        System.out.println(livros.size());
+        System.out.println();
+        
+     //   livros.size();
+       // livros.clear();
+        //livros.contains("ney");
+        //livros.isEmpty();
+        
+       
+	}
+
+}
